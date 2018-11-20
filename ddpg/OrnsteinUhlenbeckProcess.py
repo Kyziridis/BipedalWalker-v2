3ad79c4a0538dc97e90e03ed1e5cc68ae603419b
G.Kyzirids <giorgos.zapata@gmail.com>
2018-11-20 17:00:33 +0100

    Two versions of DDPG

---
+++ ddpg/OrnsteinUhlenbeckProcess.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 17:53:51 2018
+
+@author: dead
+"""
+import numpy as np
+import tensorflow as tf
+
+class OrnsteinUhlenbeckProcess:
+    """ Ornstein-Uhlenbeck Noise (original code by @slowbull)
+    """
+    def __init__(self, theta=0.15, mu=0, sigma=1, x0=0, dt=1e-2, n_steps_annealing=100, size=4):
+        self.theta = theta
+        self.sigma = sigma
+        self.n_steps_annealing = n_steps_annealing
+        self.sigma_step = - self.sigma / float(self.n_steps_annealing)
+        self.x0 = x0
+        self.mu = mu
+        self.dt = dt
+        self.size = size
+
+    def generate(self, step):
+        sigma = max(0, self.sigma_step * step + self.sigma)
+        x = self.x0 + self.theta * (self.mu - self.x0) * self.dt + sigma * np.sqrt(self.dt) * np.random.normal(size=self.size)
+        self.x0 = x
+        return x
+    
+    
+#if __name__ == '__main__':
+#    noise = OrnsteinUhlenbeckProcess(theta=0.15, mu=0, sigma=1, x0=0, dt=1e-2, n_steps_annealing=100, size=4)
